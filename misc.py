cdc9d6d535febbb48d9c15c467d907cd795ff2f5
yibo <yiboj@yibos-MacBook-Pro-2.local>
2024-04-29 15:45:29 -0500

    wv_eval

--- misc.py
+++ misc.py
@@ -4,6 +4,7 @@ import itertools
 from itertools import product
 import torch
 import os
+from scipy.linalg import hadamard, subspace_angles
 
 def get_filenames(directory_path):
     filenames = []
@@ -19,3 +20,18 @@ def get_filenames(directory_path):
             else:
                 filenames += get_filenames(directory_path + '/' + entry.name)
     return filenames
+
+
+def angle_estimate(Mb_1, Mb_2):
+    # return np.average(np.rad2deg(subspace_angles(Mb_1.T, Mb_2.T)))
+    return np.rad2deg(subspace_angles(Mb_1.T, Mb_2.T))[-1]
+
+def rank_k_approximation(matrix, k):
+    U, s, Vt = np.linalg.svd(matrix, full_matrices=False)
+    # Keep only the first k singular values and vectors
+    Uk = U[:, :k]
+    sk = np.diag(s[:k])
+    Vk = Vt[:k, :]
+    # Reconstruct the matrix using the rank-k approximation
+    approx_matrix = np.dot(Uk, np.dot(sk, Vk))
+    return approx_matrix
