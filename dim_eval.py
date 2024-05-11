5a2b845eb64152132ae8860d30f7b5e218e816c5
yibo <yiboj@yibos-MacBook-Pro-2.local>
2024-05-11 14:07:47 -0500

    more eval stuff

--- dim_eval.py
+++ dim_eval.py
@@ -76,6 +76,7 @@ if __name__ == '__main__':
 
     filenames = get_filenames(cfg.eval_path)
 
+    hamming_cos_dict = {}
     eval_list = []
     for exp_i, filename in enumerate(filenames):
 
@@ -112,34 +113,57 @@ if __name__ == '__main__':
 
             eval_list.append(eval_acc)
 
-    unembedding = model.output.weight.detach().numpy()
-    all_target_vectors = np.array(list(itertools.product([0, 1], repeat=ds.n_concept)))
+        unembedding = model.output.weight.detach().numpy()
 
-    diff_list = []
-    for base_ind in range(2**ds.n_concept):
-        diff_list.append(unembedding[base_ind,:])
-    cos_matrix_unembed = cosine_similarity(np.vstack(diff_list))
+        singular_values = np.linalg.svd(unembedding, compute_uv=False)
 
-    inner_unembed = np.vstack(diff_list) @ np.vstack(diff_list).T
+        # Plot the spectrum
+        plt.figure()
+        plt.plot(singular_values, 'bo')
+        plt.ylabel('Singular Value')
+        plt.title('Spectrum of the Unembeddings')
+        plt.grid(True)
+        plt.savefig("spectrum_n" + str(ds.n_concept) + "_d" + str(cfg.model_args.dim) + "_e" + str(exp_i) + ".png")
 
-    for target in all_target_vectors:
-        all_vectors = np.array(list(itertools.product([0, 1], repeat=ds.n_concept)))
-        hamming_cos_dict = {}
-        for vector in all_vectors:
-            # if vector[0] == target[0]:
-            hamming_distance = np.count_nonzero(target!=vector)
-            cos_list = hamming_cos_dict.get(hamming_distance, [])
-            cos_list.append(inner_unembed[int(ds.tokenize(target)), int(ds.tokenize(vector))])
-            # cos_list.append(np.linalg.norm([unembedding[int(ds.tokenize(target))] - unembedding[int(ds.tokenize(vector))]]))
-            hamming_cos_dict[hamming_distance]=cos_list
+        all_target_vectors = np.array(list(itertools.product([0, 1], repeat=ds.n_concept)))
 
-        values_array = np.zeros(len(hamming_cos_dict))
-        for key, value in hamming_cos_dict.items():
-            values_array[key] = np.average(value)
+        diff_list = []
+        for base_ind in range(2**ds.n_concept):
+            diff_list.append(unembedding[base_ind,:])
+        cos_matrix_unembed = cosine_similarity(np.vstack(diff_list))
 
-        plt.ylabel("Inner Products")
-        plt.xlabel("Hamming Distance")
-        plt.plot(np.arange(0, ds.n_concept+1), values_array[0:], label='Line 1')
+        inner_unembed = np.vstack(diff_list) @ np.vstack(diff_list).T
+
+        
+        for target in all_target_vectors:
+            all_vectors = np.array(list(itertools.product([0, 1], repeat=ds.n_concept)))
+            
+            ds.tokenize(target)
+            for vector in all_vectors:
+                if ds.tokenize(target) >= ds.tokenize(vector):
+                    # if vector[0] == target[0]:
+                    hamming_distance = np.count_nonzero(target!=vector)
+                    cos_list = hamming_cos_dict.get(hamming_distance, [])
+                    cos_list.append(inner_unembed[int(ds.tokenize(target)), int(ds.tokenize(vector))])
+                    # cos_list.append(np.linalg.norm([unembedding[int(ds.tokenize(target))] - unembedding[int(ds.tokenize(vector))]]))
+                    hamming_cos_dict[hamming_distance]=cos_list
+
+    values_array = np.zeros(len(hamming_cos_dict))
+    ste_array = np.zeros(len(hamming_cos_dict))
+    for key, value in hamming_cos_dict.items():
+        
+        values_array[key] = np.average(value)
+        ste_array[key] = stats.sem(value)
+
+        print(values_array[key], ste_array[key])
+
+    plt.figure()
+    print(hamming_cos_dict)
+    plt.ylabel("Inner Products")
+    plt.xlabel("Hamming Distance")
+    # plt.plot(np.arange(0, ds.n_concept+1), values_array[0:], label='Line 1')
+
+    plt.errorbar(np.arange(0, ds.n_concept+1), values_array[0:], yerr=ste_array)
     plt.savefig("hamming_inner_n" + str(ds.n_concept) + "_d" + str(cfg.model_args.dim) + ".png")
 
 
