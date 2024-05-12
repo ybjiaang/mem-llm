a812eabda2fdc3fc678cc84eaabcaa1d0074fb3e
yibo <yiboj@yibos-MacBook-Pro-2.local>
2024-05-12 10:58:03 -0500

    change scripts

--- scripts/run_length.sh
+++ scripts/run_length.sh
@@ -4,7 +4,8 @@ nb_concepts=(5 6 7 8)
 dims=(16 32 64 128)
 exp_ids=(0 1 2 3 4)
 lrs=(0.01 0.001)
-lengths=(8 16 24 32 40 48)
+# lengths=(8 16 24 32 40 48)
+lengths=(64 128)
 
 for NB_CONCEPTS in "${nb_concepts[@]}"
 do
